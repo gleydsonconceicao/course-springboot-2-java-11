468eef9732713d85ca4b73386ebf9d6391bac65c
Gleydson Paulo <glayck@hotmail.com>
2021-04-12 18:14:34 -0300

    Projeto criado

---
+++ src/main/java/com/example/corse/CorseApplication.java
@@ -0,0 +1,13 @@
+package com.example.corse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/corse/CorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
